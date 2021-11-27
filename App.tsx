e838ace3a44a7aaad0e4cbee36b41e57db37cb20
giancarlos1230 <gianjiron98@gmail.com>
2021-11-27 13:59:33 -0600

    pruba de app NicaSource

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import HomeNica from './src/screen/HomeNica';
+import List from './src/screen/List';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+<NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeNica} />
+        <Stack.Screen name="Lista" component={List} />
+        </Stack.Navigator>
+    </NavigationContainer>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
---
+++ src/screen/HomeNica.tsx
@@ -0,0 +1,82 @@
+import React,{useState} from 'react'
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+
+
+
+
+const HomeNica = ( {navigation }) => {
+    const [user, setUser] = useState('')
+
+    const [password, setPasword] = useState('')
+
+    const ValUser = () =>{
+        if (user==='Kevin' && password==='123'){
+            navigation.navigate('Lista')
+        }
+        else{
+            alert('Datos no correctos')
+        }
+        
+
+    }
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Usuario</Text>
+            <TextInput
+            style={styles.inputs}
+            placeholder={'Ingrese su usuario'}
+            onChangeText={setUser}
+
+            />
+            <Text style={styles.text}>Contraseña</Text>
+            <TextInput
+            style={styles.inputs}
+            placeholder={'Contraseña'}
+            onChangeText={setPasword}
+            />
+            <View style={styles.button}>
+            <Button
+            title="iniciar"
+            onPress={ValUser}
+            />
+
+            <Button
+            title="Liquidar"
+            onPress={ValUser}
+            />
+         </View>
+
+        </View>
+    )
+
+}
+
+export default HomeNica
+
+const styles = StyleSheet.create({
+    inputs: {
+        backgroundColor: '#F2F8FB',
+        borderRadius: 8,
+        padding: 10,
+        textAlign: 'center',
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#004445',
+        
+    },
+    text:{
+        fontSize:26,
+        color:'#05786A'
+    },
+    button:{
+        
+    },
+    container: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop:'50%'
+      },
+
+})
